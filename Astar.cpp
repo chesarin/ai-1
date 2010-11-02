bfa1c88ba42efe3b3e9b0c821ea9c52f4381b684
chesarin <vargasces@gmail.com>
2010-11-01 23:05:43 -0400

    This is just check in changes made so far, add_expanded_list is incomplete. This is the last part of the algorithm all the way to the bottom. Keep Working on it.

--- Astar.cpp
+++ Astar.cpp
@@ -1,19 +1,33 @@
+/*Cesar Vargas
+ *Astar.cpp
+ *Artificial Intelligence 2010
+ *Queens College, CUNY 2010
+ */
 #include "Astar.h"
 void Astar::begin(){
+  //let's create a new state from the start node using the start node
+  //double check the naming it's confusing
   start.create_state(start);
-  //  openlist.insert(start);
+  //push to the front of the test1 list, this should be the openlist
   test1.push_front(start);
+  //let's create a node called current
   node current_node;
-  //  while( !openlist.isEmpty() ){
+  //let's check there is nothing on the open list
   while( !test1.empty() ){
-    //    current_node = openlist.del();
+    //store the front of the open list on the current_node
     current_node = test1.front();
+    //delete it from the open list queue
     test1.pop_front();
+    //add current_node to the closed list
+    test2.push_front(current_node);
+    //check if the node just popped actually the goal
+    //if it is we just print it here but something else needs to be done.
     if ( isGoal(current_node,goal) ){
       cout << "We have reached the goal" << endl;
       cout << current_node << endl;
       break;
     } else {
+      //else let's expand the currnent node
       cout << "Not yet, let's expand the current_node node." << endl;
       cout << current_node << endl;
       expand(current_node);
@@ -28,10 +42,14 @@ bool Astar::isGoal(node in, node g){
     return true;
   return false;
 }
+/*expand(node input) this function takes a node and it expands it, by
+ *that we mean that it finds where the zero element is and then 
+ *it tries to see if it can move up, down, left and right. If so 
+ *then it adds all these nodes into the templist list.
+ */
 void Astar::expand(node input){
   cout << "We are in the expanding function. " << endl;
   node tempn;
-  //  Queue<node> tempq;
   list<node> templist;
   input.find_zero();
   if ( input.can_move_up() ){
@@ -39,7 +57,6 @@ void Astar::expand(node input){
     tempn.move_up();
     tempn.inc_g();
     tempn.create_state(goal);
-    //    tempq.insert(tempn);}
     templist.push_front(tempn);}
   if ( input.can_move_right() ){
     tempn = input;
@@ -47,27 +64,20 @@ void Astar::expand(node input){
     tempn.inc_g();
     tempn.create_state(goal);
     templist.push_front(tempn);}
-  //    tempq.insert(tempn);}
   if ( input.can_move_bottom() ){
     tempn = input;
     tempn.move_bottom();
     tempn.inc_g();
     tempn.create_state(goal);
     templist.push_front(tempn);}
-  //    tempq.insert(tempn);}
   if ( input.can_move_left() ){
     tempn = input;
     tempn.move_left();
     tempn.inc_g();
     tempn.create_state(goal);
     templist.push_front(tempn);}
-  //    tempq.insert(tempn);}
-  // tempq.print();
-  list<node>::iterator it;
-  cout << "templist contains:";
-  for ( it=templist.begin() ; it != templist.end() ; it++ )
-    cout << " " << *it;
-  cout << endl;
+  //let's print the templist elements
+  print_list(templist);
   //  add_expanded(tempq,input);}
 }
 ostream& operator<<(ostream &os, Square s){
@@ -138,4 +148,29 @@ bool Astar::is_member_closedlist(node in){
       sentinel=true;
   }
   return sentinel;
+}
+void Astar::print_list(list<node> &tlist){
+  list<node>::iterator it;
+  cout << "tlist contains:";
+  for ( it = tlist.begin() ; it != tlist.end() ; it++ )
+    cout << " " << *it;
+  cout << endl;
+}
+void Astar::add_expanded_list(list<node> &tlist){
+  list<node>::iterator it;
+  for ( it = tlist.begin() ; it != tlist.end() ; it++ ){
+    if ( !is_node_in_list(*it,test1) && !is_node_in_list(*it,test2) ){
+    }
+  }
+}
+bool Astar::is_node_in_list( node &tempnode, list<node> &tlist){
+  bool decision=false;
+  list<node>::iterator it;
+  for ( it = tlist.begin() ; it != tlist.end() ; it++ ){
+    if ( tempnode == *it ){
+      decision = true;
+      break;
+    }
+  }
+  return decision;
 }
