484f7ca3f847c91cf0fbaff49ee28115a5d4a7db
chesarin <vargasces@gmail.com>
2010-11-03 19:47:28 -0400

    definitions added for add_expanded and print list.

--- Astar.h
+++ Astar.h
@@ -22,7 +22,7 @@ class Astar{
   void add_expanded(list<node> &temp,node &father);
   bool is_member_openlist(node in);
   bool is_member_closedlist(node in);
-  void print_list(list<node> &tlist);
+  void print_list(list<node> &tlist, string name);
   void add_expanded_list(list<node> &tlist, node &in);
   bool is_node_in_list(node &tempnode,list<node> &tlist);
  private:
